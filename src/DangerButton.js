ef1007e658213e5ab2360ccec6c953533346a5c8
git874997967 <g874997967@gmail.com>
2017-12-16 00:49:42 -0800

    delete one necessary file

--- src/DangerButton.js
+++
@@ -1,8 +0,0 @@
-import  React, {Component} from 'react'
-import  Button from './Button'
-class DangerButton extends  Component{
-    render(){
-        return <Button/>
-    }
-}
-export default DangerButton;
